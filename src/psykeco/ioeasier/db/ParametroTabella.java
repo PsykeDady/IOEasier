343dc8c4e21069a8509eac954bcff99b0e4805a9
PsykeDady <psdady@msn.com>
2015-05-28 00:12:58 +0200

    modifiche a Entita e ParametroTabella, non influenzanti

--- src/psykeco/ioeasier/db/ParametroTabella.java
+++ src/psykeco/ioeasier/db/ParametroTabella.java
@@ -1,16 +1,38 @@
 package psykeco.ioeasier.db;
 
-public class ParametroTabella {
+public class ParametroTabella<E> {
+	public static enum Tipo{
+		VARCHAR,
+		INT,
+		DECIMAL,
+		CHAR,
+		BLOB,
+		BINARY,
+		LONGBLOB,
+		MEDIUMBLOB,
+		TINYBLOB,
+		VARBINARY,
+		DATE,
+		TIME,
+		TIMESTAMP,
+		DATETIME;
+	}//enum
 	private boolean key,not_null;
-	private Object elem;
+	private E elem;
+	private String nome_parametro;
+	private Tipo tipo;
+	private int range_tipo; //dipende daltipo
 	
-	public ParametroTabella(Object e, boolean chiave, boolean non_nullo){
+	public ParametroTabella(E e,Tipo tipo, String nome, int range_tipo , boolean chiave, boolean non_nullo){
 		elem=e;
+		this.tipo=tipo;
+		this.nome_parametro=nome;
+		this.range_tipo=range_tipo;
 		key=chiave;
 		not_null=non_nullo;
 	}//Parametro tabella
 	
-	public Object getElem (){
+	public E getElem (){
 		return elem;
 	}
 	
@@ -22,6 +44,9 @@ public class ParametroTabella {
 		return not_null;
 	}
 	
+	public String getNomeParametro(){ return nome_parametro;}
+	
+	public Tipo getTipo(){ return tipo;}
 
 	
-}//ParametroTabello
+}//ParametroTabella
