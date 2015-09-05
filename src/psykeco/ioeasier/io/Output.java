68498eb75ba09084e46a58a761cb6b917ba1eaf5
PsykeDady <psdady@msn.com>
2015-09-05 17:28:36 +0200

    aggiunta classe Output

---
+++ src/psykeco/ioeasier/io/Output.java
@@ -0,0 +1,22 @@
+package psykeco.ioeasier.io;
+
+public class Output {
+	
+	public static String matrixToString(Object[][]matrix){
+		StringBuilder sb=new StringBuilder();
+		
+		
+		
+		for(int i=0; i<matrix.length;i++){
+			sb.append('|');
+			for(int j=0;j<matrix[i].length;j++){
+				sb.append(matrix[i][j].toString());
+				if(j<matrix[i].length-1)sb.append(',');
+			}//for
+			sb.append("|\n");
+		}//for
+		
+		return sb.toString();
+	}//matrixToString
+	
+}
